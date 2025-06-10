bf9b1f758ae7ae376c7985c4b551bdda3cad41a2
Henrique Schnoor <henriqueschnoor2019@gmail.com>
2025-06-09 23:04:22 -0300

    Funcionalidade de envio e segurança

--- API/smtp/API_SMTP.php
+++ API/smtp/API_SMTP.php
@@ -1,5 +1,12 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+require __DIR__ . '/PHPMailer/src/Exception.php';
+require __DIR__ . '/PHPMailer/src/PHPMailer.php';
+require __DIR__ . '/PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
@@ -8,7 +15,6 @@ if ($json === false) {
     echo json_encode(['error' => 'Invalido o JSON enviado']);
     exit;
 }
-
 $nome = $data['nome'] ?? null;
 $email = $data['email'] ?? null;
 $mensagem = $data['mensagem'] ?? null;
@@ -18,6 +24,39 @@ if (empty($nome) || empty($email) || empty($mensagem)) {
     echo json_encode(['error' => 'Todos os campos são obrigatórios']);
     exit;
 }   
+/*--------------------------------------------------------------------------------------------------------------
+                                             SMTP PHPMailer
+----------------------------------------------------------------------------------------------------------------*/
+$smtp = new PHPMailer(true);
+
+try{
+$smtp -> SMTPDebug = DEBUG_SERVER;
+$smtp -> isSMTP();
+$smtp -> Host = 'smtp.exemple.com'; 
+$smtp -> SMTPAuth = true;
+$smtp -> Username = 'user@exemplo.com';
+$smtp -> Password = 'senha';
+$smtp -> SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$smtp -> Port = 465; 
+
+$smtp -> setForm('from@exemplo.com', 'Seu Nome ou App');
+
+$smtp -> addAddress('destinatario@exemplo.com', 'Destinatario'); // nome destinatario
+$smtp -> addReplyTo('info@exemplo.com', 'Suporte');  // a resposta irá nesse email
+$smtp -> addCC('copia@exemplo.com');
+$smtp -> addCC('copia_oculta@exemplo.com');
+
+$mail->isHTML(true);                        
+    $mail->Subject = 'Assunto do e-mail';
+    $mail->Body    = '<h1>Olá!</h1><p>Corpo em <b>HTML</b>.</p>';
+    $mail->AltBody = 'Versão texto puro para quem não lê HTML.';
+
+    $mail->send();
+    echo 'Mensagem enviada com sucesso';
+} catch (Exception $e) {
+    echo "Mensagem de erro: {$mail->ErrorInfo}";
+}
+
 
 echo json_encode([
     'status' => 'success',
